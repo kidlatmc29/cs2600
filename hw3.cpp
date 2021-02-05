49fc512cf493f8990ab5b4f5c98571eb5393b942
ozabell <kidlatmc@gmail.com>
2021-02-04 20:22:16 -0800

    wrote set operations, revising printSet fxn

--- hw3.cpp
+++ hw3.cpp
@@ -5,20 +5,20 @@
 using namespace std;
 
 const int U = 1111111111;
+const int CARDINALITY = 10;
 // Universal set U =  {0,1,2,3,4,5,6,7,8,9}
-// decimal form - 1023
 const int STOP = -1; 
+
+void printSet(int bitstring);
 
 int main()
 {
   int setA, setB = 0; 
-  int userInput;
-  int setTemp;
+  int userInput, ABunion, ABint, ABdiff, notB;
 
   while(userInput != STOP) {
     cout << "Please enter values in set A or -1 to stop: ";
     cin >> userInput;
-    // build the binary string here
     for(int i = 0; i < userInput; i++) {
       setA |= (1 << userInput);
     }
@@ -27,40 +27,48 @@ int main()
   userInput = 0; 
   cout << endl << endl;
 
-
   while(userInput != STOP) {
     cout << "Please enter values in set B or -1 to stop: ";
     cin >> userInput;
-    // build the binary string here??? 
     for(int i = 0; i < userInput; i++) {
       setB |= (1 << userInput);
     }
   }
 
- // print set a and b 
-  cout << "set A: " << setA << endl;
-  setTemp = setA; 
-  cout << "{ "; 
-  for(int i = 0; i < 10; i++) {
-    if( 1 << i & setTemp) {
-      cout << i << ", ";
+  cout << endl << endl;
+  cout << "set A: ";
+  printSet(setA);
+  cout << endl;
+
+  cout << "set B: ";
+  printSet(setB);
+  cout << endl << endl;
+
+  cout << "Union of A and B: ";
+  ABunion = setA | setB;
+  printSet(ABunion);
+
+  cout << "Intersection of A and B: ";
+  ABint = setA & setB;
+  printSet(ABint);
+
+  cout << "Difference of A and B: ";
+  notB = ~setB;
+  ABdiff = setA & notB; 
+  printSet(ABdiff);
+  cout << endl << endl;
+  
+}
+
+void printSet(int bitstring)
+{
+  string set = "{";
+
+  for(int i = 0; i < CARDINALITY; i++) {
+    if (1 << i & bitstring) {
+      set += i + ", ";
     }
   }
-  cout << "}" << endl;
- 
-  cout << "set B: " << setB << endl;
-  setTemp = setB; 
-  cout << "{ "; 
-  for(int i = 0; i < 10; i++) {
-    if( 1 << i & setTemp) {
-      cout << i << ", ";
-    }
-  }
-  cout << "}" << endl;
- // find union of a and b 
- // find interesction of a and b
- // find difference of a and b
-
-
-  return 0;
+  set += "}";
+  cout << set << endl;
 }
