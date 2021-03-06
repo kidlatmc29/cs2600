b2a2a4232901376f010d56ac8a9215ff9b6d1b29
ozabell <kidlatmc@gmail.com>
2021-03-06 11:20:38 -0800

    working on load fxn with creating the matrix

--- hw5/Graph.cpp
+++ hw5/Graph.cpp
@@ -7,7 +7,6 @@
 Graph::Graph()
 {
   numOfVertices = 0; 
-  adjMatrix = new int*[0]; // makes an empty 2-D array
 }
 
 Graph::~Graph()
@@ -22,14 +21,32 @@ Graph::~Graph()
 void Graph::load(string filename)
 {
   ifstream input; 
-  string line; 
+  int num; 
+  int x;
+  int y; 
 
   input.open(filename);
 
   if(!input.fail()) {
-    cin >> numOfVertices; // first num in file is numOfVertices
-  }
+    input >> numOfVertices; // first number is num of verticies
+    adjMatrix = new int*[numOfVertices]; // makes an empty 2-D array
+    for(int i = 0; i < numOfVertices; i++) {
+      adjMatrix[i] = new int[numOfVertices];
+    }
 
+    while(input >> x) {
+      input >> y;
+      addEdge(x, y); // filling in adjMatrix
+    }
+  }
+  input.close();
+
+}
+
+void Graph::addEdge(int x, int y)
+{
+  adjMatrix[x][y] = 1; 
+  adjMatrix[y][x] = 1;
 }
 
 void Graph::display()
--- hw5/Graph.h
+++ hw5/Graph.h
@@ -12,36 +12,27 @@ class Graph {
     int** adjMatrix = nullptr; // 2-D array for adjacency matrix of the graph
     int numOfVertices; 
     
+    void addEdge(int x, int y);
+    // helper function for load. addes a new edge into the adjMatrix. 
+
   public:
     Graph();
     // default constructor.
-    // Pre: None.
-    // Post: sets numOfVertices to 0 and creates an empty adjMatrix.
 
     ~Graph();
     // deconstructor.
-    // Pre: None.
-    // Post: deallocates adjMaxtrix and sets numOfVertices to 0.
 
     void load(string filename);
     // creates a graph using input file.
-    // Pre: Assuming filename is correct and the file is properly formatted.
-    // Post:
 
     void display();
     // displays the graph's adjacency matrix
-    // Pre:
-    // Post:
 
     void displayDFS(int vertex);
     // displays the result of a depth first search starting at the provided
     //  vertex. With a choice, picks the lower number.
-    // Pre:
-    // Post:
 
     void displayBFS(int vertex);
     // displays the result of a breadth first search starting at the provided
     // vertex. With a choice, picks the lower number.
-    // Pre:
-    // Post:
 };
